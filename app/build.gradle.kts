d44cdc345a10e511d71a3b9026b218ffbd0c4c12
Agustin Fiordaliso <fiordaa1@gmail.com>
2025-04-29 09:40:54 -0300

    [feat] cambio de color, ajustes para el dark theme y desarrollo del home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation("com.leinardi.android:speed-dial:3.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/parcial_1_am_acn4av_fiordaliso/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/parcial_1_am_acn4av_fiordaliso/ui/home/HomeFragment.java
@@ -5,27 +5,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.example.parcial_1_am_acn4av_fiordaliso.R;
 import com.example.parcial_1_am_acn4av_fiordaliso.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Este fragmento dibuja el contenido debajo del status bar
+        root.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        // Evento en el botón de "+"
+        binding.botonAgregar.setOnClickListener(view -> {
+            Toast.makeText(getContext(), "Agregar nueva transacción", Toast.LENGTH_SHORT).show();
+
+            // Crear nueva transacción (dinámicamente)
+            TextView nuevaTransaccion = new TextView(getContext());
+            nuevaTransaccion.setText("⭐ Transferencia recibida\nLunes, 21 Abr. 2025\n+$1.000,00");
+            nuevaTransaccion.setPadding(16, 16, 16, 16);
+            nuevaTransaccion.setTextColor(ContextCompat.getColor(requireContext(), R.color.white));
+
+            // Agregar al contenedor de transacciones
+            binding.listaTransacciones.addView(nuevaTransaccion);
+        });
+
         return root;
     }
 
