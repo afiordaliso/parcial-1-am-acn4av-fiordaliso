9320c1e9a45e41c659bffe2e0983ba4d1404f378
Agustin <fiordaa1@gmail.com>
2025-05-05 00:48:33 -0300

    [FEAT] Funcionalidad de publicidad inicio

--- app/src/main/java/com/example/parcial_1_am_acn4av_fiordaliso/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4av_fiordaliso/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.parcial_1_am_acn4av_fiordaliso;
 
 import android.os.Bundle;
-
+import android.view.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import android.widget.PopupMenu;
-
 import com.example.parcial_1_am_acn4av_fiordaliso.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,11 +23,26 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
+        navView.setVisibility(View.GONE);
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications)
                 .build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(navView, navController);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+
+            if (destination.getId() == R.id.navigation_home ||
+                    destination.getId() == R.id.navigation_dashboard ||
+                    destination.getId() == R.id.navigation_notifications) {
+                navView.setVisibility(View.VISIBLE);
+            } else {
+                navView.setVisibility(View.GONE);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/parcial_1_am_acn4av_fiordaliso/ui/splash/SplashFragment.java
@@ -0,0 +1,41 @@
+package com.example.parcial_1_am_acn4av_fiordaliso.ui.splash;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.parcial_1_am_acn4av_fiordaliso.R;
+
+public class SplashFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflar el layout
+        return inflater.inflate(R.layout.splash_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView ivPublicidad = view.findViewById(R.id.imagePublicidad);
+        ivPublicidad.setImageResource(R.drawable.image_publicidad);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Navigation.findNavController(view).navigate(R.id.action_splash_to_home);
+        }, 3000);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
